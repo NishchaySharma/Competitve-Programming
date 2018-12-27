2a2be66bdf37e20c1fcbc8ffbc3104e7b2b2a31e
Nishchay Sharma <42491880+NishchaySharma@users.noreply.github.com>
2018-12-27 14:18:54 +0530

    Add files via upload

--- Codechef/Qualifing to Pre-Elimination.py
+++ Codechef/Qualifing to Pre-Elimination.py
@@ -1,18 +1,10 @@
-t=int(input())
-for _ in range(t):
+for _ in range(int(input())):
     n,k=map(int,input().split())
-    s=sorted(list(map(int,input().split())),reverse=True)
-    d,unique={},[]
-    for i in s:
-        if i in d:
-            d[i]+=1
+    store=sorted(list(map(int,input().split())),reverse=True)
+    till,count=store[k-1],0
+    for i in store:
+        if i >=till:
+            count+=1
         else:
-            d[i]=1
-            unique.append(i)
-    if len(unique)<=k:
-        print(n)
-    else:
-        res=0
-        for i in range(k):
-            res+=d[unique[i]]
-        print(res)
+            break
+    print(count)
