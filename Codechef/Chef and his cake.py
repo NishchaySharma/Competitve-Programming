e7c5e0176e657627522fa1fa80f231a3dc3c40f8
Nishchay Sharma <42491880+NishchaySharma@users.noreply.github.com>
2019-01-02 11:55:22 +0530

    Add files via upload

--- Codechef/Chef and his cake.py
+++ Codechef/Chef and his cake.py
@@ -1,81 +1,38 @@
-t=int(input())
-for _ in range(t):
+for _ in range(int(input())):
     n,m=map(int,input().split())
-    s=''
+    l,tmp1,tmp2=[],[],[]
     for i in range(n):
-        s+=input()
-    s=list(s)
-    cost1,cost2,store=0,0,s[:]
-    if m%2==1:
-        tmp=0
-        for i in range(1,n*m):
-            if s[i]==s[i-1]:
-                if s[i]=='R':
-                    s[i]='G'
-                    tmp+=3
+        l.append(list(input()))
+    pre1,pre2='R','G'
+    for i in range(n):
+        tmp1.append([])
+        tmp2.append([])
+        if i!=0 and m%2==0:
+            pre1,pre2=pre2,pre1
+        for j in range(m):
+            if pre1=='R':
+                tmp1[i].append('G')
+                pre1='G'
+            else:
+                tmp1[i].append('R')
+                pre1='R'
+            if pre2=='R':
+                tmp2[i].append('G')
+                pre2='G'
+            else:
+                tmp2[i].append('R')
+                pre2='R'
+    count1,count2=0,0
+    for i in range(n):
+        for j in range(m):
+            if l[i][j]!=tmp1[i][j]:
+                if tmp1[i][j]=='R':
+                    count1+=3
                 else:
-                    s[i]='R'
-                    tmp+=5
-        s=store
-        cost1,tmp=tmp,0
-        if s[0]=='R':
-            s[0]='G'
-            tmp+=3
-        else:
-            s[0]='R'
-            tmp+=5
-        for i in range(1,n*m):
-            if s[i]==s[i-1]:
-                if s[i]=='R':
-                    s[i]='G'
-                    tmp+=3
+                    count1+=5
+            if l[i][j]!=tmp2[i][j]:
+                if tmp2[i][j]=='R':
+                    count2+=3
                 else:
-                    s[i]='R'
-                    tmp+=5
-
-        cost2=tmp
-        print(cost1 if cost1<cost2 else cost2)
-    else:
-        tmp=0
-        for i in range(1,n*m):
-            if i%m==0:
-                if s[i]!=s[i-1]:
-                    if s[i]=='R':
-                        s[i]='G'
-                        tmp+=3
-                    else:
-                        s[i]='R'
-                        tmp+=5
-            elif s[i]==s[i-1]:
-                if s[i]=='R':
-                    s[i]='G'
-                    tmp+=3
-                else:
-                    s[i]='R'
-                    tmp+=5
-        s=store
-        cost1,tmp=tmp,0
-        if s[0]=='R':
-            s[0]='G'
-            tmp+=3
-        else:
-            s[0]='R'
-            tmp+=5
-        for i in range(1,n*m):
-            if i%m==0:
-                if s[i]!=s[i-1]:
-                    if s[i]=='R':
-                        s[i]='G'
-                        tmp+=3
-                    else:
-                        s[i]='R'
-                        tmp+=5
-            elif s[i]==s[i-1]:
-                if s[i]=='R':
-                    s[i]='G'
-                    tmp+=3
-                else:
-                    s[i]='R'
-                    tmp+=5
-        cost2=tmp
-        print(cost1 if cost1<cost2 else cost2)
+                    count2+=5
+    print(min(count1,count2))
