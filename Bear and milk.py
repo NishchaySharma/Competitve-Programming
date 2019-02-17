0b65691a7d2ef1beb3809a1131675816b94d7026
Nishchay Sharma <42491880+NishchaySharma@users.noreply.github.com>
2019-02-17 21:28:19 +0530

    Delete Bear and milk.py

--- Bear and milk.py
+++
@@ -1,9 +0,0 @@
-for _ in range(int(input())):
-    n=int(input())
-    l=list(input().split())
-    flag=True
-    if n==1 and l[0]=='cookie': flag=False
-    for i in range(n-1):
-        if l[i]=='cookie' and l[i+1]!='milk': flag=False; break
-    if flag and l[-1]!='cookie': print('YES')
-    else: print('NO')
